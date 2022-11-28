97283aa2049d52db6a9c7d3f28260b562708b1d1
Cravun <KrenntKB@yahoo.com>
2022-11-28 15:52:58 +0800

    Project 1.1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
 import "./App.css";
 // === Components ===
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter></BrowserRouter>
+      <Navbar></Navbar>
     </div>
   );
 }
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav>
+      <a>
+        <h1>Logo</h1>
+      </a>
+      <div>
+        <ul>
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">Manage</a>
+          </li>
+          <li>
+            <a href="">Book</a>
+          </li>
+          <li>
+            <a href="">Login</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+export default Navbar;
